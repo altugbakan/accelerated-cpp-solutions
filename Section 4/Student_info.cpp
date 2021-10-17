3b10a834455d78a6bbcb684684d325a339f7f427
altugbakan <altugbakan@gmail.com>
2021-10-17 14:16:26 +0300

    Modifications for consistency with the book.

--- Section 4/Student_info.cpp
+++ Section 4/Student_info.cpp
@@ -1,9 +1,30 @@
 #include "Student_info.h"
 
 using std::cout;
+using std::vector;
 using std::istream;
 using std::domain_error;
+using std::sort;
 
+// code is taken from §4.1.1/53
+// compute the median of a vector<double>
+// note that calling this function copies the entire argument vector
+double median(vector<double> vec)
+{
+    typedef vector<double>::size_type vec_sz;
+
+    vec_sz size = vec.size();
+    if (size == 0)
+        throw domain_error("median of an empty vector");
+
+    sort(vec.begin(), vec.end());
+
+    vec_sz mid = size/2;
+
+    return size % 2 == 0 ? (vec[mid] + vec[mid-1]) / 2 : vec[mid];
+}
+
+// code is taken from §4.4/69
 bool compare(const Student_info& x, const Student_info& y)
 {
     return x.name < y.name;
@@ -11,37 +32,34 @@ bool compare(const Student_info& x, const Student_info& y)
 
 istream& read(istream& is, Student_info& s)
 {
-    double midterm, final, homework;
+    double midterm, final;
+    vector<double> homework;
     // read and store the student's name and final grade
     is >> s.name >> midterm >> final;
-    try {
-        read_hw(is, homework); // read and store the student's homework average
-    } catch (domain_error e) {
-        cout << e.what();
+
+    // check for end-of-file
+    if(is) {
+        read_hw(is, homework);
+        s.final_grade = 0.2 * midterm + 0.4 * final + 0.4 * median(homework);
     }
-    s.final_grade = 0.2 * midterm + 0.4 * final + 0.4 * homework;
+    
     return is;
 }
 
-istream& read_hw(istream& in, double& hw)
+// code is taken from §4.1.3/57
+istream& read_hw(istream& in, vector<double>& hw)
 {
     if (in) {
+        // get rid of previous contetns
+        hw.clear();
+
         // read homework grades
         double x;
-        double total = 0;
-        size_t count = 0;
-        while (in >> x) {
-            total += x;
-            ++count;
-        }
+        while (in >> x)
+            hw.push_back(x);
 
         // clear the stream so that input will work for the next student
         in.clear();
-
-        // calculate the average homework grade
-        if (count == 0)
-            throw domain_error("student has done no homework");
-        hw = total / count;
     }
     return in;
 }
--- Section 4/Student_info.h
+++ Section 4/Student_info.h
@@ -2,15 +2,18 @@
 #define Guard_Student_info
 
 #include <iostream>
+#include <vector>
 #include <string>
 #include <stdexcept>
+#include <algorithm>
 
 struct Student_info {
     std::string name;
     double final_grade;
 };
 
+double median(std::vector<double>);
 bool compare(const Student_info&, const Student_info&);
 std::istream& read(std::istream&, Student_info&);
-std::istream& read_hw(std::istream&, double&);
+std::istream& read_hw(std::istream&, std::vector<double>&);
 #endif
