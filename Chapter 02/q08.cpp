6b54d900c9ae7563e7330da25ad9c194e610ea14
AlexMGitHub <AlexMGitHub@Gmail.com>
2023-05-28 20:39:44 -0400

    Fix incorrect range on q08.cpp
    Range should be asymmetric range of [1, 10) per the text.

--- Chapter 02/q08.cpp
+++ Chapter 02/q08.cpp
@@ -3,10 +3,10 @@
 int main()
 {
     int product = 1;
-    // get the product of the numbers in the range [1, 10].
-    for (int i = 1; i < 11; i++)
+    // get the product of the numbers in the range [1, 10).
+    for (int i = 1; i < 10; i++)
         product *= i;
-        
+
     std::cout << product << std::endl;
 
     return 0;
